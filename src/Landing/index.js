8247162ee47ac70361fe9008be39d7910c1bc625
Agnes <agnesajema@gmail.com>
2024-06-16 23:06:18 +0300

    add changes

---
+++ src/Landing/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const Landing =()=>{
+    return(
+        <div>
+        <nav id='navbar'>
+            <img src='/Images/logo.png' alt=''/>
+            <a href='/home.index.js'>Home</a>
+            <a href='/about.index.js'>About Us</a>
+            <a href='/contacts.idex.js'>Contacts</a>
+            </nav>
+            <div id='page'>
+                <p>Shop The Best Bales For Your Second Hand Clothes </p>
+            </div>
+        </div>
+        
+
+        
+
+    );
+}
+export default Landing
