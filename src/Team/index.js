bcb8c4963188764e291d4ce760cf847fdb158554
Maureen-Njango <maureennjangombira@gmail.com>
2024-06-16 23:07:59 +0300

    added images display

---
+++ src/Team/index.js
@@ -0,0 +1,28 @@
+import "./index.css"
+
+
+// const images () =
+const Team = () =>{
+    return(
+     <div>
+        <div className="heading">
+            <h1>See What We Have...</h1></div>  
+
+        <div className="images" >
+           
+    <img src='/Images/Cotton-Dresses.webp' alt="Picture showing cotton" className="img1"/>
+    <img src='Images/Dresses-for-Girls.webp' alt="picture showing dress" className="img1"/>
+    <img src="/Images/Mencotton.webp" alt="mens clothes" className="img1"/>
+
+
+
+           
+
+        
+
+        </div>
+        </div>  
+     
+    );
+}
+export default Team
