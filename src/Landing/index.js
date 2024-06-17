a2e145a9c44a88f7551de6ad4f17c3bf0c060954
Gladwell-Wanjiku <wanjeriwanjikugladwell@gmail.com>
2024-06-17 12:57:38 +0300

    added mision

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -4,13 +4,13 @@ const Landing =()=>{
     return(
         <div id='whole'>
         <nav id='navbar'>
-            <img src='/Images/logo.png' alt=''/>
+            <img src='/Images/logo.png' id='piclogo'/>
             <a href='#'>Home</a>
             <a href='#'>About Us</a>
             <a href='#'>Contacts</a>
             </nav>
             <div id='page'>
-                <p>Shop The Best Bales <br/>For Your Second Hand Clothes Business</p>
+                <p id='paragraph'>Shop The Best Bales <br/>For Your Second Hand Clothes Business</p>
             </div>
         </div>
         
